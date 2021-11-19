5bff584ab75e531e8df79deacc29ba3bf7fbd81c
Bendi11 <bkliebmann@gmail.com>
2021-11-18 20:33:48 -0800

    Added custom parser for time editor

--- src/gui/ui/edit.rs
+++ src/gui/ui/edit.rs
@@ -1,10 +1,6 @@
 //! Widgets for the edit screen to change or create recipes
 
-use druid::{
-    text::format::ParseFormatter,
-    widget::{Flex, Label, TextBox, ValueTextBox, ViewSwitcher},
-    TextAlignment, Widget, WidgetExt,
-};
+use druid::{TextAlignment, Widget, WidgetExt, text::format::Validation, widget::{Flex, Label, TextBox, ValueTextBox, ViewSwitcher}};
 
 use crate::gui::{
     data::{
@@ -57,82 +53,115 @@ pub fn edit_widget() -> impl Widget<AppState> {
                 .expand_width(),
         )
         .with_spacer(2.0)
-        .with_child(
-            ViewSwitcher::new(
-                |data: &EditState, _env| data.time,
-                |time, _data, _env| match time {
-                    Some(_) => Flex::row()
-                        .with_child(
-                            Maybe::or_empty(|| {
-                                Flex::row()
-                                    .with_child(Label::new("Hours ").with_font(theme::SMALL_FONT))
-                                    .with_spacer(5.0)
-                                    .with_child(
-                                        ValueTextBox::new(
-                                            TextBox::new()
-                                                .with_text_alignment(TextAlignment::Center)
-                                                .with_placeholder("hours"),
-                                            ParseFormatter::new(),
-                                        )
-                                        .validate_while_editing(true)
-                                        .fix_width(50.)
-                                        .lens(EditedTime::hours),
-                                    )
-                                    .with_spacer(10.0)
-                                    .with_child(Label::new("Minutes ").with_font(theme::SMALL_FONT))
-                                    .with_spacer(5.0)
-                                    .with_child(
-                                        ValueTextBox::new(
-                                            TextBox::new()
-                                                .with_text_alignment(TextAlignment::Center)
-                                                .with_placeholder("minutes"),
-                                            ParseFormatter::new(),
-                                        )
-                                        .validate_while_editing(true)
-                                        .fix_width(50.)
-                                        .lens(EditedTime::minutes),
-                                    )
-                                    .with_spacer(10.0)
-                                    .with_child(Label::new("Seconds ").with_font(theme::SMALL_FONT))
-                                    .with_spacer(5.0)
-                                    .with_child(
-                                        ValueTextBox::new(
-                                            TextBox::new()
-                                                .with_text_alignment(TextAlignment::Center)
-                                                .with_placeholder("seconds"),
-                                            ParseFormatter::new(),
-                                        )
-                                        .validate_while_editing(true)
-                                        .fix_width(50.)
-                                        .lens(EditedTime::secs),
-                                    )
-                                    .with_spacer(20.0)
-                            })
-                            .lens(EditState::time),
-                        )
-                        .with_child(
-                            Icon::svg(&RECYCLE_ICON)
-                                .highlight_on_hover()
-                                .on_click(|ctx, data: &mut EditState, _env| {
-                                    data.time = None;
-                                    ctx.request_update();
-                                })
-                                .fix_size(35., 35.),
-                        )
-                        .boxed(),
-                    None => Icon::svg(&PLUS_ICON)
-                        .highlight_on_hover()
-                        .on_click(|_ctx, data: &mut EditState, _env| {
-                            data.time = Some(EditedTime::default())
-                        })
-                        .boxed(),
-                },
-            )
-            .expand_width()
-            .align_left()
-            .fix_height(50.),
-        )
+        .with_child(time_editor())
         .with_default_spacer()
         .lens(AppState::edit)
         .expand()
+}
+
+/// Build the root widget for the recipe time editor 
+fn time_editor() -> impl Widget<EditState> {
+    ViewSwitcher::new(
+        |data: &EditState, _env| data.time,
+        |time, _data, _env| match time {
+            Some(_) => Flex::row()
+                .with_child(
+                    Maybe::or_empty(|| {
+                        Flex::row()
+                            .with_child(Label::new("Hr").with_font(theme::SMALL_FONT))
+                            .with_spacer(5.0)
+                            .with_child(
+                                ValueTextBox::new(
+                                    TextBox::new()
+                                        .with_text_alignment(TextAlignment::Center)
+                                        .with_placeholder("hours"),
+                                        TimeEditorFormatter,
+                                )
+                                .fix_width(50.)
+                                .lens(EditedTime::hours),
+                            )
+                            .with_spacer(10.0)
+                            .with_child(Label::new("Min").with_font(theme::SMALL_FONT))
+                            .with_spacer(5.0)
+                            .with_child(
+                                ValueTextBox::new(
+                                    TextBox::new()
+                                        .with_text_alignment(TextAlignment::Center)
+                                        .with_placeholder("minutes"),
+                                        TimeEditorFormatter,
+                                )
+                                .fix_width(50.)
+                                .lens(EditedTime::minutes),
+                            )
+                            .with_spacer(10.0)
+                            .with_child(Label::new("Sec").with_font(theme::SMALL_FONT))
+                            .with_spacer(5.0)
+                            .with_child(
+                                ValueTextBox::new(
+                                    TextBox::new()
+                                        .with_text_alignment(TextAlignment::Center)
+                                        .with_placeholder("seconds"),
+                                    TimeEditorFormatter,
+                                )
+                                
+                                .fix_width(50.)
+                                .lens(EditedTime::secs),
+                            )
+                            .with_spacer(20.0)
+                    })
+                    .lens(EditState::time),
+                )
+                .with_child(
+                    Icon::svg(&RECYCLE_ICON)
+                        .highlight_on_hover()
+                        .on_click(|ctx, data: &mut EditState, _env| {
+                            data.time = None;
+                            ctx.request_update();
+                        })
+                        .fix_size(35., 35.),
+                )
+                .boxed(),
+            None => Icon::svg(&PLUS_ICON)
+                .highlight_on_hover()
+                .on_click(|_ctx, data: &mut EditState, _env| {
+                    data.time = Some(EditedTime::default())
+                })
+                .boxed(),
+        },
+    )
+    .expand_width()
+    .align_left()
+    .fix_height(50.)
+}
+
+/// A structure implementing [Formatter](druid::text::format::Formatter) to parse a `u8` from the input box
+#[derive(Clone, Copy, Debug)]
+struct TimeEditorFormatter;
+
+impl druid::text::format::Formatter<u8> for TimeEditorFormatter {
+    fn format(&self, value: &u8) -> String {
+        value.to_string()
+    }
+
+    fn format_for_editing(&self, value: &u8) -> String {
+        value.to_string()
+    }
+
+    fn validate_partial_input(&self, input: &str, sel: &druid::text::Selection) -> Validation {
+        if input[sel.range()].is_empty() {
+            Validation::success()
+        } else {
+            match input[sel.range()].parse::<u8>() {
+                Ok(_) => Validation::success(),
+                Err(e) => Validation::failure(e)
+            }
+        }
+    }
+
+    fn value(&self, input: &str) -> Result<u8, druid::text::format::ValidationError> {
+        if input.is_empty() {
+            return Ok(0)
+        }
+        input.parse().map_err(|e| druid::text::format::ValidationError::new(e))
+    }
 }
