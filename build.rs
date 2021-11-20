8105fd420aade3418ac6ffbd44e01ee22850354d
Bendi11 <bkliebmann@gmail.com>
2021-11-19 18:57:50 -0800

    Added bundlefor macos to build.rs

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 #[cfg(windows)]
 extern crate winres;
 
-#[cfg(all(debug_assertions, windows))]
+#[cfg(windows)]
 fn main() {
     println!("cargo:rerun-if-changed=assets/icon.ico,assets/icon.png");
     if let Err(e) = winres::WindowsResource::new()
@@ -10,6 +10,16 @@ fn main() {
     {
         eprintln!("Error adding windows resource file to executable: {}", e);
     }
+}
+
+#[cfg(all(not(debug_assertions), target_os="macos"))]
+fn main() {
+    std::fs::create_dir_all("./recipier.app/Contents").expect("Failed to create app bundle directory");
+    std::fs::create_dir_all("./recipier.app/Contents/MacOS").expect("Failed to create app bundle binary directory");
+    std::fs::create_dir_all("./recipier.app/Contents/Resources").expect("Failed to create app bundle resources directory");
+
+    std::fs::copy("assets/Info.plist", "./recipier.app/Contents/Info.plist").expect("failed to copy Info.plist file");
+    std::fs::copy("assets/icon.icns", "./recipier.app/Contents/Resources/icon.icns").expect("failed to copy icon.icns file");
 }
 
 #[cfg(not(windows))]
